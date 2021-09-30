c47ce37bbe6b5005d9c3f3c57b6aa757bdea25f0
Gabriel Rios <mr.gabrielrios@gmail.com>
2021-09-30 16:40:30 -0400

    Addition of potential and virtual potential temperature fields.

--- dev/bin/functions.py
+++ dev/bin/functions.py
@@ -8,7 +8,7 @@ Description:    This script contains functions to derive certain meteorological 
 # Imports
 import datetime, netCDF4 as nc, numpy as np, os, pandas as pd, time, xarray as xr
 
-def gamma(data):
+def lapse_rate(data):
     '''
     Calculate lapse rate in K km^-1.
 
@@ -31,13 +31,16 @@ def gamma(data):
     temp['height'] = 0
     temp.values = np.full(temp.values.shape, np.nan)
     gamma = xr.concat([gamma, temp], dim='height').sortby('height')
+    # Assign to Dataset
     data = data.assign(lapse_rate=gamma)
+    # Assign units
+    data['lapse_rate'].attrs = {'units': 'K km^-1'}
     
     return data
 
 def pressure(data):
     '''
-    
+    Calculate atmospheric pressure over all given times and heights.
 
     Parameters
     ----------
@@ -60,13 +63,11 @@ def pressure(data):
     for i in range(0, len(data.height.values)):
         # Get current iteration height
         curr_ht = data.height.values[i]
-        print(curr_ht)
         # Preload values into concatenated xArray Dataset
         if i < 2:
             temp = data['pressure'].loc[{'height': curr_ht}]
         # Derive pressure for heights above 100m
         # Current model: https://home.chpc.utah.edu/~hallar/Thermo/Lectures/Lecture6.pdf
-        # Note: equation needs work to address weird values
         else:
             prev_ht = data.height.values[i-1]
             # Re-use "temp" to reduce computation time
@@ -77,7 +78,57 @@ def pressure(data):
         pressures.append(temp)
     # Concatenate data and assign as a data variable to main Dataset
     data['pressure'] = xr.concat(pressures, dim='height')
+    # Assign units
+    data['pressure'].attrs = {'units': 'hPa'}
     
     return data    
     
-        
+def potential_temperature(data):
+    '''
+    Calculates potential temperature for given Dataset.
+
+    Parameters
+    ----------
+    data : xArray Dataset
+        xArray Dataset containing all observed data AND pressure data.
+
+    Returns
+    -------
+    data : TYPE
+        xArray Dataset containing potential temperature in addition to pre-existing variables.
+
+    '''
+    
+    # Calculate potential temperature using surface pressure as p_0 term
+    data['potential_temperature'] = data['temperature'] * (data['pressure'].sel(height=0)/data['pressure']) ** (287/1004)
+    # Assign units
+    data['potential_temperature'].attrs = {'units': 'K'}
+    
+    return data
+
+def virtual_potential_temperature(data):
+    '''
+    Calculates virtual potential temperature for given Dataset.
+
+    Parameters
+    ----------
+    data : xArray Dataset
+        xArray Dataset containing all observed data AND pressure data.
+
+    Returns
+    -------
+    data : TYPE
+        xArray Dataset containing virtual potential temperature in addition to pre-existing variables.
+
+    '''
+    
+    # Calculate partial pressure of water vapor in hPa
+    e = (data['vapor_density']/1000 * 461.5 * data['temperature'])/100
+    # Calculate virtual potential temperature, using a mixing ratio of 0.622
+    data['virtual_potential_temperature'] = data['temperature']/(1 - (1-0.622)*e/data['pressure'])
+    # Assign units
+    data['virtual_potential_temperature'].attrs = {'units': 'K'}
+    
+    return data
+    
+    
