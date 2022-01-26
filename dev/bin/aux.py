2306daf864846cad3545bcb87581d210224f0471
Gabriel Rios <mr.gabrielrios@gmail.com>
2022-01-26 11:58:24 -0500

    Addition of function to plot NARR data. Will be used for case studies.

--- dev/bin/aux.py
+++ dev/bin/aux.py
@@ -5,7 +5,7 @@ Path:           ~/bin/aux.py
 Description:    Store functions used for miscellaneous and non-essential tasks - namely, manuscript supplemental information.
 """
 
-import cartopy.crs as ccrs, cartopy.io.img_tiles as cimgt, cartopy.feature as cfeature, matplotlib as mpl, matplotlib.pyplot as plt, numpy as np, os, pandas as pd
+import cartopy.crs as ccrs, cartopy.io.img_tiles as cimgt, cartopy.feature as cfeature, datetime, matplotlib as mpl, matplotlib.pyplot as plt, numpy as np, os, pandas as pd, xarray as xr
 
 from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 
@@ -50,6 +50,7 @@ def basemap(bound_box, pad=0.05, sat_img=False, high_res=False):
     lat_formatter = LatitudeFormatter()
     ax.xaxis.set_major_formatter(lon_formatter)
     ax.yaxis.set_major_formatter(lat_formatter)
+    
     ''' Add satellite or base imagery. '''
     if sat_img:
         # Define imagery resolution
@@ -123,8 +124,76 @@ def nyc_building_heights(bound_box):
     
     return grid
     
+def narr(date):
+    '''
+    Plot NARR data for a given date over the Northeast. 
+    '''
+    
+    ''' Build preliminary map definitions. '''
+    # Plot New York City central coordinates and define spatial extent parameter
+    central_latitude, central_longitude, extent = [40.7128, -74.0060, 15]
+    # Get spatial extent bounds
+    lat_max, lon_min, lat_min, lon_max = [central_latitude + extent, 
+                                          central_longitude - extent,
+                                          central_latitude - extent, 
+                                          central_longitude + extent]
+    # Define the cartographic projection for the maps
+    proj = ccrs.Orthographic(central_latitude=np.nanmean([lat_max, lat_min]),
+                             central_longitude=np.nanmean([lon_max, lon_min]))
+    
+    ''' Load data for 2021. '''
+    # Define directory path where NARR data is stored
+    dir_ = '/Volumes/UBL Data/data/narr'
+    # Build list of .nc files in the directory
+    files = [os.path.join(dir_, file) for file in os.listdir('/Volumes/UBL Data/data/narr')]
+    # Concatenate and load into a single xArray Dataset
+    data = xr.open_mfdataset(files)
+    
+    # Clip data to spatial extent defined
+    clipped = data.where(((data.lat > lat_min) & (data.lat < lat_max) 
+                          & (data.lon > lon_min) & (data.lon < lon_max)), 
+                         drop=True)
+    # Limit temporal extent of data to prevent other dates from messing with the data range
+    clipped = clipped.sel(time=date)
+    # Get 2m air temperature
+    temp_2m = clipped['air']
+    # Get mean sea level pressure
+    mslp = clipped['prmsl'] // 100
+    
+    ''' Plot data. '''
+    fig, ax = plt.subplots(dpi=300, subplot_kw={'projection': proj})
+    # Padding to prevent visibility of clipped spatial extent
+    pad = 5
+    # Set spatial extent
+    ax.set_extent([lon_min+pad, lon_max-pad, lat_min+pad, lat_max-pad])
+    # Set colormap for temperature
+    cmap = 'Spectral_r'
+    # Normalize temperature data
+    norm = mpl.colors.Normalize(vmin=np.nanmin(clipped['air'].values),
+                                vmax=np.nanmax(clipped['air'].values))
+    # Plot temperature
+    temp_2m_plot = ax.contourf(clipped.lon, clipped.lat, temp_2m.values, 
+                               levels=32, norm=norm, cmap=cmap, transform=ccrs.PlateCarree())
+    # Plot mean sea level pressure
+    mslp_plot = ax.contour(clipped.lon, clipped.lat, mslp.values, 
+                           levels=10, colors='k', transform=ccrs.PlateCarree(), zorder=4)
+    # Add labels for the contours
+    mslp_labels = ax.clabel(mslp_plot, mslp_plot.levels, 
+                            fmt='%0.0f', inline=True, inline_spacing=25, fontsize=8, zorder=5)
+    # Define the colorbar
+    colorbar = fig.colorbar(temp_2m_plot, norm=norm, cmap=cmap, extend='both')
+    # Add miscelleanous mapping features
+    ax.add_feature(cfeature.OCEAN, zorder=2)
+    ax.add_feature(cfeature.LAKES)
+    ax.coastlines('10m', zorder=3)
+    
+    ax.set_title(date, loc='left')
+    
+    return data
+    
 if __name__ == '__main__':
     # Print New York City
-    grid = nyc_building_heights([(40.915, -74.256), (40.492, -73.699)])
+    # grid = nyc_building_heights([(40.915, -74.256), (40.492, -73.699)])
+    xr_data = narr(r'2021-08-11 0:00:00')
     
 
